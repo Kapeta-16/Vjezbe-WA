b4cc2e8095d1f0e65f012755fa4f634a448c1b69
Kapeta-16 <murija11162004@gmail.com>
2025-10-21 11:37:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express =  require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3005;
+
+app.get('/', (req, res) => {
+    console.log('pozvan je GET endpoint!');
+    res.send({ poruka: 'Pozdrav iz Express.js posluzitelja'});
+})
 
 app.listen(PORT, error => {
     if (error) {
         console.error('Ne mogu startati... neda mi se...');
     } else {
-        console.log('Express.js posluzitelj slusa na pprtu ${PORT}')
+        console.log(`Express.js posluzitelj slusa na portu ${PORT}`)
     }
 });
