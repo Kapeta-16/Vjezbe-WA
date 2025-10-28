8ae58e706b61a89e784b3cffcfe2a047c1e016d6
Kapeta-16 <murija11162004@gmail.com>
2025-10-28 11:23:17 +0100

    added 2 htmls

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ const express =  require('express');
 const app = express();
 const PORT = 3005;
 
+let korsnici = [
+    {id:1, ime:'Marko', prezime:'Maric'},
+    {id:2, ime:'Ana', prezime:'Anic'},
+    {id:3, ime:'Pero', prezime:'Peric'}
+]
+
+
 app.get('/', (req, res) => {
     console.log('pozvan je GET endpoint!');
-    res.send({ poruka: 'Pozdrav iz Express.js posluzitelja'});
+    res.send({poruka: 'Pozdrav iz Expressa'});
 })
+
+
+app.get('/', (req, res) => {
+    console.log('pozvan je GET endpoint!');
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+
+app.get('/about', (req, res) => {
+    console.log('pozvan je GET endpoint!');
+    res.sendFile(__dirname + '/public/about.html');
+})
+
 
 app.listen(PORT, error => {
     if (error) {
