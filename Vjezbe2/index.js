a6d9877afdcae420a52d54b48a7c0dd910e70f8f
Kapeta-16 <murija11162004@gmail.com>
2025-10-29 17:04:41 +0100

    .

---
+++ Vjezbe2/index.js
@@ -0,0 +1,54 @@
+import express from 'express';
+let PORT = 3000;
+let app = express();
+
+let pizze = [
+    {id: 1, naziv: 'Margheritta', cijena: 9},
+    {id: 2, naziv: 'Capricciosa', cijena: 12},
+    {id: 3, naziv: 'Vegetariana', cijena: 18},
+    {id: 4, naziv: 'Quatro formaggi', cijena: 15},
+    {id: 5, naziv: 'Salami', cijena: 13},
+]
+
+app.get("/", (req, res) => {
+    console.log('Pozvana GET / ruta')
+    res.send('Pozdrav')
+})
+
+
+app.get("/pizze", (req, res) => {
+    res.json(pizze)
+})
+
+app.get("/pizze/:naziv", (req, res) => {
+    let naziv_pizze = req.params.naziv
+    console.log('Trazim pizzu:', naziv_pizze)
+    //-X GET http://localhost:3000/pizze/Vegetariana
+
+    let trazena_pizza = pizze.find(pizza => {
+        return naziv_pizze == pizza.naziv;
+    })
+
+    res.json(trazena_pizza)
+})
+
+app.post("/pizze", (req, res) => {
+    let nova_pizza = req.body;
+    console.log('Podaci', nova_pizza);
+    pizze.push(nova_pizza);
+    res.json(pizze);
+})
+
+app.delete("/pizze/:id", (req, res) => {
+    let brisanje_id = req.params.id;
+    console.log(brisanje_id);
+    res.send('Endpoint radi...')
+})
+
+app.listen(PORT, error => {
+    if (error) {
+        console.error('Doslo je od greske u pokretanju ')
+
+    }
+    console.log(`Aplikacija slusa na portu ${PORT}`)
+})
